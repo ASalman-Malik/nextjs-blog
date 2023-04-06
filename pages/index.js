363d9349ca0721e8d9856d260c12cc64be0e8cb3
Ahmad Salman Malik <36794358+ASalman-Malik@users.noreply.github.com>
2023-04-06 15:22:17 +0530

    3

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,20 @@ export default function Home() {
       <Head>
         <title>My Page Title</title>
         <meta name="description" content="This is my page description" />
-        <meta property="og:title" content="My Page Title" />
+        <meta property="og:title" content="My Blog | Home" />
         <meta property="og:description" content="This is my page description" />
-        <meta property="og:image" content="https://example.com/my-image.jpg" />
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=694&q=80"
+        />
         <meta property="og:url" content="https://example.com/my-page" />
         <meta name="twitter:title" content="My Page Title" />
         <meta
           name="twitter:description"
           content="This is my page description"
         />
-        <meta name="twitter:image" content="https://example.com/my-image.jpg" />
+        https://images.unsplash.com/photo-1566438480900-0609be27a4be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=694&q=80"
+        />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <h5>Blog</h5>
